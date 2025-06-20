2f6ec3b6e89ce725208c6bf44ef395e3ba688ca9
ayushsrivastava-code <ayush.srivastava@justchargetechnologies.com>
2025-06-20 13:32:41 +0000

    comparision of datatypes

---
+++ 01.basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // the comparision and the equality operator works different in the javascript 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+

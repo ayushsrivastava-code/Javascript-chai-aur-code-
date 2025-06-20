66911f474d838529714ae328e03464ff644bc39a
ayushsrivastava-code <ayush.srivastava@justchargetechnologies.com>
2025-06-20 14:14:53 +0000

    datatype summary

---
+++ 01.basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// primitive 
+// 7 types : String, number. boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol()
+
+
+//  Non-primitive 
+// Array, Objects , functions
+
+ 
